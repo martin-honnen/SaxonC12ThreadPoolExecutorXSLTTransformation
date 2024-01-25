36fd06c0d6a849ddf427355f0d32295f34d1fee8
Martin Honnen <martin.honnen@gmx.de>
2024-01-25 22:13:44 +0100

    Updating to Saxon HE 12.4.2

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def transform(saxon_processor, xslt30_executable, file_name):
         return e.message
 def thread_pool_test():
     with PySaxonProcessor() as saxon_processor:
+        print(saxon_processor.version)
         xslt30_compiler = saxon_processor.new_xslt30_processor()
 
         try:
