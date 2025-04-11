d93fad19eef495fea7058ef34c226224a0dfd6be
Martin Honnen <martin.honnen@gmx.de>
2025-04-11 11:32:24 +0200

    main.py and main2.py both work with SaxonC 12.5 HE under Windows but under Linux main.py crashes with "Fatal error: StackOverflowError: Enabling the yellow zone of the stack did not make any stack space available."

---
+++ main2.py
@@ -0,0 +1,45 @@
+import concurrent
+import os
+from concurrent.futures import ThreadPoolExecutor
+
+from saxonche import PySaxonProcessor
+from saxonche import PySaxonApiError
+
+def transform(saxon_processor, xslt30_executable, file_name):
+    try:
+        saxon_processor.attach_current_thread
+        #xdm_node = saxon_processor.parse_xml(xml_file_name=f'input/{file_name}')
+        #xslt30_executable.set_global_context_item(xdm_item=xdm_node)
+        xslt30_executable.apply_templates_returning_file(source_file=f'input/{file_name}', output_file=f'output/{file_name}')
+        return True
+    except PySaxonApiError as e:
+        return e.message
+    finally:
+        saxon_processor.detach_current_thread
+def thread_pool_test():
+    with PySaxonProcessor() as saxon_processor:
+        print(saxon_processor.version)
+        saxon_processor.set_configuration_property('http://saxon.sf.net/feature/xinclude-aware', 'on')
+        xslt30_compiler = saxon_processor.new_xslt30_processor()
+
+        try:
+            xslt30_executable = xslt30_compiler.compile_stylesheet(stylesheet_file='identity.xsl')
+            with ThreadPoolExecutor(max_workers=4) as executor:
+                futures = {executor.submit(transform, saxon_processor, xslt30_executable, file): file for file in os.listdir('input')}
+                for future in concurrent.futures.as_completed(futures):
+                    file = futures[future]
+                    result = future.result()
+                    if (result == True):
+                        print(f'File {file} transformed successfully!')
+                    else:
+                        print(f'File {file} transformation failed with {result}')
+
+        except PySaxonApiError as e:
+            print(f'XSLT compilation failed: {e}')
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    thread_pool_test()
+
